d89e65e9bd8722c15dce7521315f5acd2e64ecd4
chirag-unision <sharmacj123@gmail.com>
2023-09-01 16:53:28 +0530

    created login-screen: input field and submit button

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,12 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import { TextInput, Button } from "@react-native-material/core"
+import React, { useState } from 'react'
 
 export default function Login() {
+  const [loading, setLoading] = useState(false);
+  const [emailCheck, setEmailCheck]= useState(false);
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.wrapper}>
+      <View style={styles.cotainer}>
+        <Text style={[styles.text, {textAlign: 'center', fontSize: 20}]}>Login</Text>
+        <TextInput variant="standard" label="Email" color="#fff" style={{ margin: 16, width: 300 }} />
+        {emailCheck && <TextInput variant="standard" label="Enter One-Time Password" color="#fff" style={{ margin: 16, width: 300 }} />}
+        <Button
+          title="Login"
+          loading={loading}
+          loadingIndicatorPosition="overlay"
+        />
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1E1E1E',
+    height: Dimensions.get('screen').height
+  },
+  cotainer: {
+    padding: 20,
+    margin: 0,
+    backgroundColor: 'black'
+  },
+  text: {
+    color: '#fff'
+  }
+})
